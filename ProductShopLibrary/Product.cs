63a8ec94e8a7288d73d85c98b014638c3c7e9eeb
Никита Раненко <nranenko@bk.ru>
2022-10-19 11:19:31 +0700

    Перенос файлов

---
+++ ProductShopLibrary/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductShopLibrary
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int QuantityInStock { get; set; }
+        public byte[] Image { get; set; }
+    }
+}
