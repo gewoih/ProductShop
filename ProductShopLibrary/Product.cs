55b477d4bae981aa7952712fcca03c22d2eba2f9
Никита Раненко <nranenko@bk.ru>
2022-10-19 13:13:19 +0700

    Добавлен id

--- ProductShopLibrary/Product.cs
+++ ProductShopLibrary/Product.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ProductShopLibrary
+﻿namespace ProductShopLibrary
 {
     public class Product
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Category { get; set; }
         public string Description { get; set; }
