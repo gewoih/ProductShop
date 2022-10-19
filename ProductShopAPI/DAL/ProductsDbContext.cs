d1e22fd1ad9bb0f83e515511ffa033bdd76620ba
Никита Раненко <nranenko@bk.ru>
2022-10-19 13:13:53 +0700

    Добавлен конструктор

--- ProductShopAPI/DAL/ProductsDbContext.cs
+++ ProductShopAPI/DAL/ProductsDbContext.cs
@@ -6,5 +6,9 @@ namespace ProductShopAPI.DAL
     public class ProductsDbContext : DbContext
     {
         public DbSet<Product> Products { get; set; }
+
+        public ProductsDbContext(DbContextOptions<ProductsDbContext> options) : base(options)
+        {
+        }
     }
 }
