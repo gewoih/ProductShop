acd9bcf23112b867ee4b64a4bf62f4fa3c383ab0
Никита Раненко <nranenko@bk.ru>
2022-10-22 20:59:51 +0700

    Сервис для работы с продуктами

---
+++ ProductShop/Services/ProductsService.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using ProductShopLibrary;
+
+namespace ProductShop.Services
+{
+    public class ProductsService
+    {
+        public List<Product> GetAllProducts()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                List<Product> products;
+                
+                string response = client.GetStringAsync("https://localhost:7019/api/Products/getProducts").Result;
+                products = JsonConvert.DeserializeObject<List<Product>>(response);
+
+                return products;
+            }
+        }
+    }
+}
