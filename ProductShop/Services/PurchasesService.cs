335deff63affc90dba746488d6e60f546706612b
Никита Раненко <nranenko@bk.ru>
2022-10-27 00:57:21 +0700

    Сервис для покупок

---
+++ ProductShop/Services/PurchasesService.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using ProductShopLibrary.Purchases;
+using System.Net;
+using System.Text;
+
+namespace ProductShop.Services
+{
+    public class PurchasesService
+    {
+        public async Task<bool> TrySubmitPurchase(Purchase purchase)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var content = new StringContent(JsonConvert.SerializeObject(purchase), Encoding.UTF8, "application/json");
+                var response = await client.PostAsync("https://localhost:7019/api/Purchases/create_purchase", content);
+                
+                if (response.IsSuccessStatusCode)
+                    return true;
+                    
+                return false;
+            }
+        }
+    }
+}
