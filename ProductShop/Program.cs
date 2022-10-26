1b6e5a1b9b6fd4f2b6ee4ef7a88ace78cd6393ca
Никита Раненко <nranenko@bk.ru>
2022-10-27 00:58:24 +0700

    Регистрация сервиса для покупок

--- ProductShop/Program.cs
+++ ProductShop/Program.cs
@@ -1,11 +1,4 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.JSInterop;
 using ProductShop.Services;
-using ProductShopAPI.DAL;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +7,8 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor()
     .AddHubOptions(o => o.MaximumReceiveMessageSize = 1024 * 1024 * 32);
 
-builder.Services.AddTransient<ProductsService>();
+builder.Services.AddScoped<ProductsService>();
+builder.Services.AddScoped<PurchasesService>();
 builder.Services.AddScoped<LocalStorage>();
 
 var app = builder.Build();
